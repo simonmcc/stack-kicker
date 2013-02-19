04c6ea3a37eb85b445c49ca09a02bc0b5b2654c2
Simon McCartney <simon.mccartney@hp.com>
2013-02-19 19:54:53 +0000

    As generated by methadone

---
+++ lib/stack-kicker.rb
@@ -0,0 +1,7 @@
+require "stack-kicker/version"
+
+module Stack
+  module Kicker
+    # Your code goes here...
+  end
+end
---
+++ lib/stack-kicker/version.rb
@@ -0,0 +1,5 @@
+module Stack
+  module Kicker
+    VERSION = "0.0.1"
+  end
+end
